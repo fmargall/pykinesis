0b13ff0a9fceb1222f7f26388f889fd79186b76b
François Margall <fr.margall@proton.me>
2026-03-23 11:07:52 +0100

    ✨ feat: add direct acces for user to KCubeSolenoid class

--- pykinesis/__init__.py
+++ pykinesis/__init__.py
@@ -1,3 +1,6 @@
+# High level convenience class
+from .kcube_solenoid import KCubeSolenoid
+
 from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("pykinesis")
