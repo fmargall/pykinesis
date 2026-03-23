712b0b8a22987290f2e2e8c4e7b4f26dcc04e58a
François Margall <fr.margall@proton.me>
2026-03-23 11:04:49 +0100

    ✨ feat(errors): add complete list of error codes

--- pykinesis/core/_errors.py
+++ pykinesis/core/_errors.py
@@ -29,9 +29,42 @@ class _ErrorCode(IntEnum):
     FT_OK = 0x00
 
     # FTDI and Communication errors
-    FT_InvalidHandle   = 0x01
-    FT_DeviceNotFound  = 0x02
-    FT_DeviceNotOpened = 0x03
+    FT_InvalidHandle         = 0x01
+    FT_DeviceNotFound        = 0x02
+    FT_DeviceNotOpened       = 0x03
+    FT_IOError               = 0x04
+    FT_InsufficientResources = 0x05
+    FT_InvalidParameter      = 0x06
+    FT_DeviceNotPresent      = 0x07
+    FT_IncorrectDevice       = 0x08
+
+    # Device libraries errors
+    FT_NoDLLLoaded          = 0x10
+    FT_NoFunctoinsAvailable = 0x11
+    FT_FunctionNotAvailable = 0x12
+    FT_BadFunctionPointer   = 0x13
+    FT_GenericFunctionFail  = 0x14
+    FT_SpecificFunctionFail = 0x15
+
+    # General DLL control errors
+    TL_ALREADY_OPEN           = 0x20
+    TL_NO_RESPONSE            = 0x21
+    TL_NOT_IMPLEMENTED        = 0x22
+    TL_FAULT_REPORTED         = 0x23
+    TL_INVALID_OPERATION      = 0x24
+    TL_DISCONNECTING          = 0x28
+    TL_FIRMWARE_BUG           = 0x29
+    TL_INITIALIZATION_FAILURE = 0x2A
+    TL_INVALID_CHANNEL        = 0x2B
+
+    # Motor specific errors
+    TL_UNHOMED                    = 0x25
+    TL_INVALID_POSITION           = 0x26
+    TL_INVALID_VELOCITY_PARAMETER = 0x27
+    TL_CANNOT_HOME_DEVICE         = 0x2C
+    TL_JOG_CONTINOUS_MODE         = 0x2D
+    TL_NO_MOTOR_INFO              = 0x2E
+    TL_CMD_TEMP_UNAVAILABLE       = 0x2F
 
     @property
     def message(self) -> str:
