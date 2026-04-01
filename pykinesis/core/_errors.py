035cf5dac5b64f75911971c26496999ee848f7a4
François Margall <fr.margall@proton.me>
2026-04-01 13:17:46 +0200

    📝 doc(errors): add error help messages

--- pykinesis/core/_errors.py
+++ pykinesis/core/_errors.py
@@ -40,7 +40,7 @@ class _ErrorCode(IntEnum):
 
     # Device libraries errors
     FT_NoDLLLoaded          = 0x10
-    FT_NoFunctoinsAvailable = 0x11
+    FT_NoFunctionsAvailable = 0x11
     FT_FunctionNotAvailable = 0x12
     FT_BadFunctionPointer   = 0x13
     FT_GenericFunctionFail  = 0x14
@@ -70,8 +70,41 @@ class _ErrorCode(IntEnum):
     def message(self) -> str:
         return {
             # FTDI and Communication errors
-            _ErrorCode.FT_InvalidHandle   : "The FTDI functions have not been initialized"       ,
-            _ErrorCode.FT_DeviceNotFound  : "The device could not be found"                      ,
-            _ErrorCode.FT_DeviceNotOpened : "The device must be opened before it can be accessed" 
+            _ErrorCode.FT_InvalidHandle         : "The FTDI functions have not been initialized"            ,
+            _ErrorCode.FT_DeviceNotFound        : "The device could not be found"                           ,
+            _ErrorCode.FT_DeviceNotOpened       : "The device must be opened before it can be accessed"     ,
+            _ErrorCode.FT_IOError               : "An I/O Error has occured in the FTDI chip"               ,
+            _ErrorCode.FT_InsufficientResources : "There are Insufficient resources to run this application",
+            _ErrorCode.FT_InvalidParameter      : "An invalid parameter has been supplied to the device"    ,
+            _ErrorCode.FT_DeviceNotPresent      : "The device is no longer present"                         ,
+            _ErrorCode.FT_IncorrectDevice       : "The device detected does not match that expected."       ,
+
+            # Device libraries errors
+            _ErrorCode.FT_NoDLLLoaded          : "The library for this device could not be found",
+            _ErrorCode.FT_NoFunctionsAvailable : "No functions available for this device"        ,
+            _ErrorCode.FT_FunctionNotAvailable : "The function is not available for this device" ,
+            _ErrorCode.FT_BadFunctionPointer   : "Bad function pointer detected"                 ,
+            _ErrorCode.FT_GenericFunctionFail  : "The function failed to complete succesfully"   ,
+            _ErrorCode.FT_SpecificFunctionFail : "The function failed to complete succesfully"   ,
+
+            # General DLL control errors
+            _ErrorCode.TL_ALREADY_OPEN           : "Attempt to open a device that was already open"                        ,
+            _ErrorCode.TL_NO_RESPONSE            : "The device has stopped responding"                                     ,
+            _ErrorCode.TL_NOT_IMPLEMENTED        : "The function has not been implemented"                                 ,
+            _ErrorCode.TL_FAULT_REPORTED         : "The device has reported a fault"                                       ,
+            _ErrorCode.TL_INVALID_OPERATION      : "The function could not be completed at this time"                      ,
+            _ErrorCode.TL_DISCONNECTING          : "The function could not be completed because the device is disconnected",
+            _ErrorCode.TL_FIRMWARE_BUG           : "The firmware has thrown an error"                                      ,
+            _ErrorCode.TL_INITIALIZATION_FAILURE : "The device failed to initialize"                                       ,
+            _ErrorCode.TL_INVALID_CHANNEL        : "An invalid channel address was supplied"                               ,
+
+            # Motor specific errors
+            _ErrorCode.TL_UNHOMED                    : "The device cannot perform this function until it has been homed"                   ,
+            _ErrorCode.TL_INVALID_POSITION           : "The function cannot be performed as it would result in an illegal position"        ,
+            _ErrorCode.TL_INVALID_VELOCITY_PARAMETER : "An invalid velocity parameter was supplied. The velocity must be greater than zero",
+            _ErrorCode.TL_CANNOT_HOME_DEVICE         : "The device does not support homing. Check the limit switch parameters are correct" ,
+            _ErrorCode.TL_JOG_CONTINOUS_MODE         : "An invalid jog mode was supplied for the jog function"                             ,
+            _ErrorCode.TL_NO_MOTOR_INFO              : "There is no Motor Parameters available to convert Real World Units"                ,
+            _ErrorCode.TL_CMD_TEMP_UNAVAILABLE       : "Command temporarily unavailable. Device may be busy"
         
         }.get(self, "Unknown Kinesis SDK error.")
